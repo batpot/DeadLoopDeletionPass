ab05de54ca9e41b2d8ad051cf83ccdbec3a374e2
Marija Gru <magicinator@gmail.com>
2024-09-04 10:49:52 +0200

    deleted redundant files

--- DeadLoopDeletion/DeadLoopDeletionPass.cpp
+++ DeadLoopDeletion/DeadLoopDeletionPass.cpp
@@ -2,32 +2,51 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/Analysis/LoopInfo.h"
-#include "llvm/Analysis/LoopUnrollAnalyzer.h"
-#include "llvm/Analysis/LoopPass.h"
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
-#include "llvm/Transforms/Utils/LoopUtils.h"
-#include "llvm/Transforms/Utils.h"
-#include "llvm/Transforms/Utils/LoopPeel.h"
-#include "llvm/Transforms/Utils/LoopSimplify.h"
-#include "llvm/Transforms/Utils/SizeOpts.h"
-#include "llvm/Transforms/Utils/UnrollLoop.h"
-
-#include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/CFG.h"
+#include "llvm/IR/PatternMatch.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/LoopUnrollAnalyzer.h"
+#include "llvm/Analysis/LoopAnalysisManager.h"
+#include "llvm/Analysis/LoopPass.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils.h"
+#include "llvm/Transforms/Utils/LoopUtils.h"
+#include "llvm/Transforms/Utils/LoopPeel.h"
+#include "llvm/Transforms/Utils/LoopSimplify.h"
+#include "llvm/Transforms/Utils/SizeOpts.h"
+#include "llvm/Transforms/Utils/UnrollLoop.h"
+#include "llvm/ADT/Statistic.h"
+
+
+#include <iostream>
 
 using namespace llvm;
 
-
-
 namespace {
     struct DeadLoopDeletionPass : public LoopPass {
+        std::vector<BasicBlock *> LoopBasicBlocks;
+        std::unordered_map<Value *, Value *> VariablesMap;
+    	Value *LoopCounter, *LoopBound;
+    	bool isLoopBoundConst;
+    	int BoundValue;
+
         static char ID;
         DeadLoopDeletionPass() : LoopPass(ID) {}
-
+        
+        void mapVariables(Loop *L)
+  		{
+    		Function *F = L->getHeader()->getParent();
+    		for (BasicBlock &BB : *F) {
+      			for (Instruction &I : BB) {
+        			if (isa<LoadInst>(&I)) {
+          				VariablesMap[&I] = I.getOperand(0);
+        			}
+      			}
+    		}
+  		}
 
         // brisemo petlje koje 
         // - nikad se ne izvrsavaju 
@@ -35,144 +54,115 @@ namespace {
         // - nemaju bocne efekte
 
         //isLoopDead --> se izvrsavaju ali ne uticu na rezultat  && nemaju bocne efekte
-        
-        // bool isLoopNeverExecuted(Loop *L){
-        //     // todo
-        // }
 
-        // bool hasSideEffects(Loop *L) {
-        //     // todo 
-        // }
 
-        bool isLoopDead(Loop* L, SmallVectorImpl<BasicBlock *> &ExitingBlocks, bool &Changed, BasicBlock *ExitBlock, BasicBlock *Preheader) {
-            BasicBlock::iterator BI = ExitBlock->begin();
-            bool AllEntriesInvariant = true;
-            bool AllOutgoingValuesSame = true;
+        // brisemo one petlje koje imaju u sebi load ili store (random funkcija)
+        // bool isLoopDead(Loop* L) {
+        //     BasicBlock *Header = L->getHeader();
+        //     BasicBlock *Latch = L->getLoopLatch();
+        //     SetVector<BasicBlock*> LoopBlocks;
             
-            // phi node predstavlja 
-            while (PHINode *P = dyn_cast<PHINode>(BI)) {
-                Value *incoming = P->getIncomingValueForBlock(ExitingBlocks[0]);
+        //     // Pronađi sve blokove u telu petlje osim zaglavlja i latch
+        //     for (auto *Block : L->blocks()) {
+        //         if (Block == Header || Block == Latch) {
+        //             continue; // Preskoči zaglavlje i latch
+        //         }
+        //         LoopBlocks.insert(Block);
+        //     }
 
-                bool AllOutgoingValuesSame = true;
-                for (auto It = ExitingBlocks.begin() + 1; It != ExitingBlocks.end(); ++It) {
-                    if (incoming != P->getIncomingValueForBlock(*It)) {
-                        // AllOutgoingValuesSame = false;
-                        return false;
-                    }
-                }
+        //     // Prolazak kroz sve blokove tela petlje
+        //     for (auto *Block : LoopBlocks) {
+        //         for (auto &I : *Block) {
+        //             if (isa<LoadInst>(I) || isa<StoreInst>(I) || isa<CallInst>(I)) {
+        //                 errs() << "Instrukcija " << I.getOpcodeName() << " se nalazi u telu petlje u bloku: " << Block->getName() << "\n";
+        //                 return true;
+        //             }
+        //         }
+        //     }
 
-                if (Instruction *I = dyn_cast<Instruction>(incoming))
-                    if (!L->makeLoopInvariant(I, Changed, Preheader->getTerminator())) {
-                        // AllEntriesInvariant = false;
-                        return false;
-                    }
+        //     return true;
+        // }         
+           
+        
+        // brise petlju skroz 
+        void DeleteDeadLoop(Loop *L) {
+            std::vector<BasicBlock *> LoopBodyBasicBlocks(LoopBasicBlocks.size() - 2);
+            std::copy(LoopBasicBlocks.begin() + 1, LoopBasicBlocks.end() - 1, LoopBodyBasicBlocks.begin()); // [)
 
-                ++BI;
+            L->getLoopPreheader()->getTerminator()->setSuccessor(0, L->getExitBlock());
+            
+            for(BasicBlock *BB : LoopBasicBlocks) {
+                BB->eraseFromParent();
             }
-
-            for (auto *Block : L->blocks()) {
-                for (auto &I : *Block) {
-                    if (I.mayHaveSideEffects()) {
-                        return false;
-                    }
-                }
-            }
-            return true;
         }
-        bool makesNoProgramStateChanges(Loop *L) {
-            // todo
-        }
-
-
-        // TODO
-        void deleteDeadLoop(Loop *L, DominatorTree &DT, LoopInfo& LI, ScalarEvolution &SE)
+        
+        // PROVERA ZA LOOP KOJI SE NIKAD NE IZVRSAVA
+        
+        void findLoopCounterAndBound(Loop *L)
         {
-            auto *Preheader = L->getLoopPreheader();
-            assert(Preheader && "Preheader should exist!");
-
-            SE.forgetLoop(L);
-
-            auto *ExitBlock = L->getUniqueExitBlock();
-            if (!ExitBlock) {
-                errs() << "Error: Loop does not have a unique exit block! Cannot proceed with the transformation.\n";
-                return;
-            }
-            if (!L->hasDedicatedExits()) {
-                errs() << "Error: Loop does not have dedicated exits! The loop might need to be simplified first.\n";
-                return;
-            }
-
-            // prespajamo instrukcije tako da se terminirajuca instrukcija u preheader-u nastavlja direktno na exitblock loop-a
-            Preheader->getTerminator()->replaceUsesOfWith(L->getHeader(), ExitBlock);
-
-            BasicBlock::iterator BI = ExitBlock->begin();
-            while (PHINode *P = dyn_cast<PHINode>(BI)) {
-                int PredIndex = 0;
-                P->setIncomingBlock(PredIndex, Preheader);
-
-                // kad brisemo petlju, moramo da obrisemo PHI instrukcije koje su se objedinjavale iz razlicitih exit blokova da bi IR ostao validan
-                // brise se unazad zbog siftovanja indeksa kada se brisu PHI cvorovi
-                unsigned numIncomingValues = P->getNumIncomingValues();
-
-                while (numIncomingValues > 1) {
-                    P->removeIncomingValue(numIncomingValues - 1, false);
-                    --numIncomingValues;
-                }
-
-                //PHINode treba da ima samo jednog prethodnika i incoming vrednost za PredIndex blok treba da bude Preheader
-                bool hasSingleIncomingValue = (numIncomingValues == 1);
-                bool hasCorrectIncomingBlock = (P->getIncomingBlock(PredIndex) == Preheader);
-
-                if (!hasSingleIncomingValue || !hasCorrectIncomingBlock)
-                    return;
-                BI++;
-            }
-
+        	for (Instruction &I : *L->getHeader()) {
+        		if (isa<ICmpInst>(&I)) {
+        			LoopCounter = VariablesMap[I.getOperand(0)];
+        			if (ConstantInt *ConstInt = dyn_cast<ConstantInt>(I.getOperand(1))) {
+        				isLoopBoundConst = true;
+        				BoundValue = ConstInt->getSExtValue();
+        			}
+        		}
+        	}
         }
+        
+        bool isNeverExecuted(Loop *L)
+        {
+        	// TODO uopsti da ceo uslov bude neki false condition kao sto je i<0, mozda cak i na manje od 0??
+			if(BoundValue == 0)
+           	{
+            	//errs() << "isNeverExecuted: true" << "\n";
+           		return true;
+            }
+            else
+            {
+            	//errs() << "isNeverExecuted: false, BoundValue=" << BoundValue << "\n";
+              	return false;
+			}
+        }
+        
 
-        bool deleteLoopIfDead(Loop* L, DominatorTree &DT, LoopInfo& LI, ScalarEvolution &SE) {
-            assert(L->isLCSSAForm(DT) && "Expected LCSSA!");
-
+        bool deleteLoopIfDead(Loop* L) {
             BasicBlock *Preheader = L->getLoopPreheader();
             if (!Preheader || !L->hasDedicatedExits())
                 return false;
 
             // subloops TODO
 
-            // loop mora da ima exit block
             BasicBlock *ExitBlock = L->getUniqueExitBlock();
-            SmallVector<BasicBlock *, 4> ExitingBlocks;
-            L->getExitingBlocks(ExitingBlocks);
-
             if (!ExitBlock) {
                 return false; // required single exit block
             }
 
-            // Finally, we have to check that the loop really is dead.
-            bool Changed = false;
-            if(!isLoopDead(L, ExitingBlocks, Changed, ExitBlock, Preheader))
-                return false; // loop is not invariant, cannot delete
+            // if(!isLoopDead(L))
+            //     return false; // loop is not invariant, cannot delete
 
+            if(isNeverExecuted(L))
+            {
+            	//errs() << "loop u f-ji: " << L->getHeader()->getParent()->getName().str()	<< " se nikad ne izvrsava" << "\n";
+            	DeleteDeadLoop(L);
+            	return true;
+            }
 
-            // if (isLoopNeverExecuted(L) && makesNoProgramStateChanges(L) && !hasSideEffects(L)) {
-            //     L->getParentLoop()->removeChildLoop(L);
-            //     return true;
-            // }
-            deleteDeadLoop(L, DT, LI, SE);
-            return false;
+			return false;
         }
 
+
         bool runOnLoop(Loop *L, LPPassManager &LPM) override {
-            printf("[SOON] This pass will delete dead loops!\n");
+            //printf("[SOON] This pass will delete dead loops!\n");  
+            mapVariables(L); 
+            findLoopCounterAndBound(L);         
 
-            DominatorTree &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
-            LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-
-            bool Result = deleteLoopIfDead(L, DT, LI);
-
-            // analysis?
+            LoopBasicBlocks = L->getBlocksVector();
+            // printf("eeeeeeeeee: %d\n", (int)LoopBasicBlocks.size());
 
             // Result == obrisana petlja promenjen IR
+            bool Result = deleteLoopIfDead(L);
             return Result;
         }
 
